973e870712efd5ffb812a5a45ac185a78ba6f742
Douglas Daniel Dutra Lopes <ddlopes@aglminas.local>
2021-06-21 15:32:37 -0300

    feat: add route Preload

--- SolarSystem/src/routes/index.tsx
+++ SolarSystem/src/routes/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import Welcome from '../pages/Welcome'
 import BottomTabs from './BottomTabs'
 import UserIdentification from '../pages/UserIdentification'
+import Preload from '../pages/Preload'
 
 const Routes: React.FC = () => {
   const Stack = createStackNavigator()
@@ -11,7 +12,9 @@ const Routes: React.FC = () => {
   return(
     <Stack.Navigator
       screenOptions={{headerShown: false}}
+      initialRouteName='Preload'
     >
+      <Stack.Screen name='Preload' component={Preload} />
       <Stack.Screen name='Welcome' component={Welcome} />
       <Stack.Screen name='UserIdentification' component={UserIdentification} />
       <Stack.Screen name='BottomTabs' component={BottomTabs} />
