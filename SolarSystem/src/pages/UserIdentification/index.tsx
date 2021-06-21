2189d7c6df76eea2c6d2d10b20f6899de6b0feb7
Douglas Daniel Dutra Lopes <ddlopes@aglminas.local>
2021-06-21 13:54:58 -0300

    feat: create page UserIdentification

--- SolarSystem/src/pages/UserIdentification/index.tsx
+++ SolarSystem/src/pages/UserIdentification/index.tsx
@@ -1,11 +1,72 @@
 import React from 'react'
+import { useNavigation } from '@react-navigation/core'
+import { Alert } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { Wrapper } from './styles'
+//styles
+import { 
+  Wrapper,
+  Background,
+  Content,
+  Gradient
+} from './styles'
+
+//icons
+import { Feather } from '@expo/vector-icons'
+
+//Components, Global and theme
+import { Button, Text } from '../../global'
+import { SizedBox, Input } from '../../components'
+import { useTheme } from 'styled-components'
+import { useState } from 'react'
 
 const UserIdentification: React.FC = () => {
+  const { size, colors } = useTheme()
+  const navigation = useNavigation()
+  const [name, setName] = useState('');
+
+  const height = size.height * 1.4
+
+  async function navigateToBottomTabs() {
+    if(!name)
+      return Alert.alert('Me diz como podemos chamar você')
+
+    try {
+      await AsyncStorage.setItem('@solarsystem:user', name)
+    } catch(err) {
+      Alert.alert('Não foi possivel salvar seu nome')
+    }
+
+    navigation.navigate('BottomTabs')
+  }
+  
   return(
     <Wrapper>
+      <Background height={height} width={height} />
+      <Content>
+        <Text size='subtitle' center> 
+          Como podemos {'\n'}chamar você?
+        </Text>
+        <SizedBox height={60} />
+        <Input
+          textAlign='center' 
+          placeholder='Digite um nome'
+          placeholderTextColor={colors.textOpacity}
 
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+        
+        <SizedBox height={20} />
+        <Button
+          disabled={!!name ? false : true} 
+          onPress={() => navigateToBottomTabs()}
+        >
+          <Gradient>
+            <Text bold>Confirmar</Text>
+          </Gradient>
+        </Button>
+      </Content>
     </Wrapper>
   )
 }
--- SolarSystem/src/pages/UserIdentification/styles.ts
+++ SolarSystem/src/pages/UserIdentification/styles.ts
@@ -1,5 +1,34 @@
 import styled from 'styled-components/native'
 
+import BackgroundSolarSystem from '../../assets/backgrounds/background-solar-system.svg'
+import { LinearGradient } from 'expo-linear-gradient'
+
 export const Wrapper = styled.View`
   flex: 1;
+  background-color: ${({theme}) => theme.colors.background};
+  align-items: center;
+  justify-content: center;
+`
+
+export const Background = styled(BackgroundSolarSystem)`
+  position: absolute;
+  right: -35%;
+  top: -20%;
+`
+export const Content = styled.View`
+  width: 60%;
+`
+
+export const Gradient = styled(LinearGradient).attrs(({theme}) => ({
+  colors: theme.gradients.button,
+  start: { x: 0, y:1 },
+  end: { x: 1, y:0 }
+}))`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 32px;
+  border-radius: 8px;
+  flex-direction: row;
+  width: 100%;
 `
